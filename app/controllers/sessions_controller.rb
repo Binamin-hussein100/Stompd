bdfe47f46f3d3cd2e8d256b24a1aed4f75a5b713
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2023-02-23 12:00:08 +0300

    feat: create session for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+    def create
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            token = JWT.encode({user_id: user.id},Rails.application.secrets.secret_key_base)
+            render json: {token: token}
+        else
+            render json: {error: "Invalid password or password"}, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        render json: {message: 'Successfully Logged in'}
+    end
 end
