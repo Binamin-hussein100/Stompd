76cef6afd16c74d1b60b6dcc90b7a01f09279d4f
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2023-02-24 23:57:11 +0300

    updates

--- db/migrate/20230221193603_create_users.rb
+++ db/migrate/20230221193603_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :first name
+      t.string :last name
       t.string :email
       t.string :password_digest
       t.string :user_type
