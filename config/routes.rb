0fae5531041557e3d9892c01fd06caa8ab7d8e56
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2023-02-28 09:52:29 +0300

    all entity routes using scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :shippings
+  resources :order_products
+  resources :payments
+  resources :orders
+  resources :cart_sneakers
   resources :carts
   resources :users
   resources :sneakers
