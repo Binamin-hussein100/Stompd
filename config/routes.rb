796d462b86357d69d29c94a80b6a5bfdb2773642
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2023-03-01 11:15:03 +0300

    feat: add authenticate route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
-  resources :shippings
-  resources :order_products
-  resources :payments
-  resources :orders
-  resources :cart_sneakers
-  resources :carts
-  resources :users
-  resources :sneakers
+  namespace :api do
+    namespace :v1 do
+      resources :shippings
+      resources :order_products
+      resources :payments
+      resources :orders
+      resources :cart_sneakers
+      resources :carts
+      resources :users
+      resources :sneakers
 
-  post 'signup', to: 'users#create'
-  post 'login', to: 'sessions#create'
+      post 'signup', to: 'users#create'
+      post 'login', to: 'sessions#create'
+      post 'authenticate', to: 'authentication#create'
+    end
+  end
 end
