c72436d25bd92a4fcaba4df50003c1298d3e69b1
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2023-03-01 14:32:39 +0300

    test: test user params for create method

--- spec/requests/auth_spec.rb
+++ spec/requests/auth_spec.rb
@@ -3,7 +3,14 @@ require 'rails_helper'
 RSpec.describe "Auths", type: :request do
   describe "POST /authenticate" do
     it 'authenticates the client' do
-      post '/api/v1/authenticate'
+      post '/api/v1/authenticate', params: {username: 'BinAmin', password:'123445'}
+
+      expect(response).to have_http_status(:created)
+      # expect(response.content_type).to eq('application/json')
+      # response_body = JSON.parse(response.body)
+      expect(response_body).to eq({
+        'token' => '123'
+      })
     end
   end
 end
