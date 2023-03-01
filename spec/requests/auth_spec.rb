104850f99d7a79ab0ddc5cf0427f4ce04a535b39
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2023-03-01 14:36:59 +0300

    test: response code and body are correct

--- spec/requests/auth_spec.rb
+++ spec/requests/auth_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe "Auths", type: :request do
       post '/api/v1/authenticate', params: {username: 'BinAmin', password:'123445'}
 
       expect(response).to have_http_status(:created)
-      # expect(response.content_type).to eq('application/json')
-      # response_body = JSON.parse(response.body)
+      expect(response.content_type).to eq('application/json; charset=utf-8')
+      response_body = JSON.parse(response.body)
       expect(response_body).to eq({
         'token' => '123'
       })
