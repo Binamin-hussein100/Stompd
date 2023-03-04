4d42ad4c711b36a21ec2e50c74cb66ada04cddc0
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2023-03-04 11:01:28 +0300

    create test for user auth create

--- spec/requests/auth_spec.rb
+++ spec/requests/auth_spec.rb
@@ -12,5 +12,25 @@ RSpec.describe "Auths", type: :request do
         'token' => '123'
       })
     end
+    it 'returns error when username is missing' do 
+      post '/api/v1/authenticate', params: {password:'123445'}
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(response.content_type).to eq('application/json; charset=utf-8')
+      response_body = JSON.parse(response.body)
+      expect(response_body).to eq({
+        "error"=>"param is missing or the value is empty: username"
+      })
+
+    end
+    
+    it 'return error when password is missing' do
+      post '/api/v1/authenticate', params: {username: 'BinAmin'}
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(response.content_type).to eq('application/json; charset=utf-8')
+      response_body = JSON.parse(response.body)
+      expect(response_body).to eq({
+        "error"=>"param is missing or the value is empty: password"
+      })
+    end
   end
 end
