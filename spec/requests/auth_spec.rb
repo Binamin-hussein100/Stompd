2e2b09194a15e4223aeba7185de71bea7859455b
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2023-03-05 12:19:35 +0300

    add auth testing with token

--- spec/requests/auth_spec.rb
+++ spec/requests/auth_spec.rb
@@ -1,36 +1,48 @@
 require 'rails_helper'
 
-RSpec.describe "Auths", type: :request do
-  describe "POST /authenticate" do
+describe 'Authentication', type: :request do
+  before do 
+    @user = User.create(username: 'Binamin')
+  end
+  describe 'POST /authenticate' do
+
     it 'authenticates the client' do
-      post '/api/v1/authenticate', params: {username: 'BinAmin', password:'123445'}
+      post '/api/v1/authenticate', params: { username: @user.username, password: 'Password1' }
 
       expect(response).to have_http_status(:created)
       expect(response.content_type).to eq('application/json; charset=utf-8')
       response_body = JSON.parse(response.body)
       expect(response_body).to eq({
-        'token' => '123'
+        'token' => 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.DiPWrOKsx3sPeVClrm_j07XNdSYHgBa3Qctosdxax3w'
       })
     end
-    it 'returns error when username is missing' do 
-      post '/api/v1/authenticate', params: {password:'123445'}
-      expect(response).to have_http_status(:unprocessable_entity)
-      expect(response.content_type).to eq('application/json; charset=utf-8')
-      response_body = JSON.parse(response.body)
-      expect(response_body).to eq({
-        "error"=>"param is missing or the value is empty: username"
-      })
 
-    end
-    
-    it 'return error when password is missing' do
-      post '/api/v1/authenticate', params: {username: 'BinAmin'}
+    it 'returns error when username is missing' do
+      post '/api/v1/authenticate', params: { password: 'Password1' }
+
       expect(response).to have_http_status(:unprocessable_entity)
       expect(response.content_type).to eq('application/json; charset=utf-8')
       response_body = JSON.parse(response.body)
       expect(response_body).to eq({
-        "error"=>"param is missing or the value is empty: password"
+        'error' => 'param is missing or the value is empty: username'
       })
+    end
+
+    it 'returns error when password is missing' do
+      post '/api/v1/authenticate', params: { username: 'BookSeller99' }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(response.content_type).to eq('application/json; charset=utf-8')
+      response_body = JSON.parse(response.body)
+      expect(response_body).to eq({
+        'error' => 'param is missing or the value is empty: password'
+      })
+    end
+
+    it 'returns error when password is incorrect' do
+      post '/api/v1/authenticate', params: { username: @user.username, password: 'incorrect' }
+
+      expect(response).to have_http_status(:unauthorized)
     end
   end
 end
